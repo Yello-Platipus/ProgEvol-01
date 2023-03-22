dfb29aaa792bb2fb38e75e59fabbdfec0a18aa9f
Yello-Platipus <danieltinez1@gmail.com>
2023-03-22 14:27:54 +0100

    peta

--- src/Cositas/Mutacion/MutacionInsercion.java
+++ src/Cositas/Mutacion/MutacionInsercion.java
@@ -12,14 +12,12 @@ public class MutacionInsercion extends Mutacion{
     public void mutar(Individuo ind, double probMutacion) {
         int tamCromosoma = ind.getCromosoma().length;
         int nMutaciones = (int) (tamCromosoma * probMutacion);
-        for(int i = 0; i < nMutaciones; i++){
-            int pos1 = (int) (Math.random() * tamCromosoma);
-            int pos2 = (int) (Math.random() * tamCromosoma);
-            Object aux = ind.getCromosoma()[pos1];
-            for(int j = pos1; j < pos2; j++){
-                ind.getCromosoma()[j] = ind.getCromosoma()[j+1];
-            }
-            ind.getCromosoma()[pos2] = aux;
+        int pos1 = (int) (Math.random() * tamCromosoma);
+        int pos2 = (int) (Math.random() * tamCromosoma);
+        Object aux = ind.getCromosoma()[pos1];
+        for(int j = pos1; j < pos2; j++){
+            ind.getCromosoma()[j] = ind.getCromosoma()[j+1];
         }
+        ind.getCromosoma()[pos2] = aux;
     }
 }
