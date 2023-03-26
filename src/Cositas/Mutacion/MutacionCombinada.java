d4ab9646b475732307c81243545d94cf66cde48c
YeFan <fye02@ucm.es>
2023-03-26 18:42:36 +0200

    oopsie

--- src/Cositas/Mutacion/MutacionCombinada.java
+++ src/Cositas/Mutacion/MutacionCombinada.java
@@ -14,17 +14,19 @@ public class MutacionCombinada extends Mutacion{
 
     @Override
     public void mutar(Individuo ind, double probMutacion) {
-        ArrayList<Individuo> lista = new ArrayList<Individuo>();
-        for(int i = 0; i < 3; i++){
-            Individuo aux = ind.clonar();
-            lista.add(aux);
-        }
-        new MutacionInsercion().mutar(lista.get(0), 1);
-        new MutacionIntercambio().mutar(lista.get(1), 1);
-        new MutacionInversion().mutar(lista.get(2), 1);
-        Collections.sort(lista);
-        for(int i = 0; i < ind.getCromosoma().length; i++){
-            ind.setCromosoma(i, lista.get(0).getCromosoma()[i]);
+        if(Math.random() < probMutacion){
+            ArrayList<Individuo> lista = new ArrayList<Individuo>();
+            for(int i = 0; i < 3; i++){
+                Individuo aux = ind.clonar();
+                lista.add(aux);
+            }
+            new MutacionInsercion().mutar(lista.get(0), 1);
+            new MutacionIntercambio().mutar(lista.get(1), 1);
+            new MutacionInversion().mutar(lista.get(2), 1);
+            Collections.sort(lista);
+            for(int i = 0; i < ind.getCromosoma().length; i++){
+                ind.setCromosoma(i, lista.get(0).getCromosoma()[i]);
+            }
         }
     }
 }
