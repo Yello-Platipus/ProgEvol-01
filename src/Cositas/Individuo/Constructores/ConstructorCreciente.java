5161ead1b556e335f40f212e8f538c769660afcf
usuario_local <fye02@ucm.es>
2023-04-14 15:47:26 +0200

    Cruce hehco, toca testearlo

--- src/Cositas/Individuo/Constructores/ConstructorCreciente.java
+++ src/Cositas/Individuo/Constructores/ConstructorCreciente.java
@@ -6,15 +6,20 @@ public class ConstructorCreciente extends Constructor {
     @Override
     public void construir(Tree n, int prof) {
         if(prof != maxProf) {
-            if(Math.random() < 0.5)
+            if(Math.random() < 0.5){
                 n = new Tree(funciones[(int) (Math.random() * funciones.length)]);
-            else
+                n.esTerminal = false;
+            }
+            else{
                 n = new Tree(terminales[(int) (Math.random() * terminales.length)]);
+                n.esTerminal = true;
+            }
             construir(n.left, prof + 1);
             construir(n.right, prof + 1);
         }
         else{
             n = new Tree(terminales[(int) (Math.random() * terminales.length)]);
+            n.esTerminal = true;
         }
     }
 
